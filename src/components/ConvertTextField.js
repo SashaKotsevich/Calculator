19705a92f4eb7b853d1f77cd0083690ce0bcb214
SashaKotsevich <kotsevich.a@yandex.ru>
2019-05-15 18:04:19 +0300

    Clear branch

--- src/components/ConvertTextField.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import styles from "../styles/convertTextField.css";
-class ConvertTextField extends Component {
-	render() {
-		return <input type="text" className={styles.text_field} />;
-	}
-}
-
-export default ConvertTextField;
--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux";
-import standart from "./standart";
-import convert from "./convert";
-export default combineReducers({ standart, convert });
