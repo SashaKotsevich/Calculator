1acd636445d23931f4e4822176d26bb2c1278335
SashaKotsevich <kotsevich.a@yandex.ru>
2019-05-17 11:27:17 +0300

    rebuild history tab layout

--- src/components/History.js
+++ src/components/History.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import HistoryItem from "./HistoryItem";
 import styles from "../styles/history.css";
+import { Scrollbars } from "react-custom-scrollbars";
 class History extends Component {
 	render() {
 		return (
-			<section className={styles.history_wrapper}>
+			<Scrollbars className={styles.history_wrapper}>
 				<HistoryItem />
 				<HistoryItem />
 				<HistoryItem />
 				<HistoryItem />
-			</section>
+				<HistoryItem />
+				<HistoryItem />
+			</Scrollbars>
 		);
 	}
 }
--- src/components/HistoryItem.js
+++ src/components/HistoryItem.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import styles from "../styles/histotyItem.css";
+const types = ["/images/expression_icon.png", "/images/convert_icon.png"];
 const fakeData = {
 	date: new Date().toLocaleString(),
-	type: "Calculate",
+	type: "",
 	expression: "10+(5-2*4)/3",
 	result: 9,
 };
 class HistoryItem extends Component {
+	arrayRandElement = () => {
+		let rand = Math.floor(Math.random() * types.length);
+		return types[rand];
+	};
 	render() {
 		return (
 			<section className={styles.item_wrapper}>
-				<label className={styles.type}>Operation: {fakeData.type}</label>
-				<label className={styles.date}>Date: {fakeData.date}</label>
-				<label>{fakeData.expression}</label>
-				<label className={styles.result}>= {fakeData.result}</label>
+				<img className={styles.type} src={this.arrayRandElement()} alt="err" />
+				<section className={styles.description}>
+					<label className={styles.date}>Date: {fakeData.date}</label>
+					<label>{fakeData.expression}</label>
+					<label className={styles.result}>= {fakeData.result}</label>
+				</section>
 			</section>
 		);
 	}
